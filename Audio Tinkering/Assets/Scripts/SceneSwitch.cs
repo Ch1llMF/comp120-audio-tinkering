772ee1bea03eb39575a8ddce108fd4c1b1569839
Josh <ji229970@falmouth.ac.uk>
2019-12-03 03:22:06 +0000

    Implemented script which allowed me to switch scenes by clicking the buttons.

---
+++ Audio Tinkering/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitch : MonoBehaviour
+{
+	public void GoToMenuScene () 
+	{
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void GoToPlayScene ()
+	{
+		SceneManager.LoadScene("Play");
+	}
+
+	public void GoToSettingsScene ()
+	{
+		SceneManager.LoadScene("Settings");
+	}
+}
